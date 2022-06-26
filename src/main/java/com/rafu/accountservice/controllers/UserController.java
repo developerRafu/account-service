9d598d272e19c0198e63d0d0ea70835413ae480b
rafael.henrique <rafael.henrique@techlead.com.br>
2022-06-26 19:44:17 -0300

    removendo import desnecessário.

--- src/main/java/com/rafu/accountservice/controllers/UserController.java
+++ src/main/java/com/rafu/accountservice/controllers/UserController.java
@@ -7,7 +7,6 @@ import com.rafu.accountservice.models.rest.UserResponse;
 import com.rafu.accountservice.services.AuthService;
 import com.rafu.accountservice.services.UserService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
